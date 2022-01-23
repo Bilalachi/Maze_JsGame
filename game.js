732f2eef2e74c5c9cb78f2f5db323a04c588a379
Bilal Achi <Bilal.achi94@gmail.comgit remote add origin https://github.com/Bilalachi/dog_dating_websitegit push -u origin maingit config --global user.email Bilal.achi94@gmail.com>
2022-01-23 08:21:08 +0200

    Adjusted the Win function and added some comments

--- game.js
+++ game.js
@@ -1,76 +1,82 @@
 window.onload = function () {
 
+    /*Once the cursor leaves the start div the game starts */
     var start = document.getElementById("start");
     start.onmouseover = startGame;
     start.onmouseleave = startGame;
     start.onclick = newGame;
 
+    /*Once the cursor reaches the end div without touching borders the game end with a win */
     var end = document.getElementById("end");
     end.onmouseover = win;
 
+    /*Once the cursor touches the wall the game ends with a loss */
     var boundaries = document.getElementsByClassName("boundary");
-    for (var i = 0; i < boundaries.length; i++){
+    for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].onmouseover = lose;
     }
 
+    /*In case the cursor was outside the maze the game stops until player reaches start div */
     var game = document.getElementById("game");
     game.onmouseleave = outOfBound;
 
+    /*Using the boundary example div to keep scores */
     var score = 0;
-    var scores =  document.getElementsByClassName("boundary example")[0];
+    var scores = document.getElementsByClassName("boundary example")[0];
     scores.innerHTML = `${score}`;
 
+    /*Touch the start div to change the status and in case of previous loss reset the game by removing the red color starting the game for a new try */
     function startGame() {
 
 
-        if (document.getElementById("status").innerText != "Dont Hit The Boundaries"){
-        document.getElementById("status").innerText = "Dont Hit The Boundaries";
+        if (document.getElementById("status").innerText != "Dont Hit The Boundaries") {
+            document.getElementById("status").innerText = "Dont Hit The Boundaries";
         }
-        for (var i = 0; i < boundaries.length; i++){
+        for (var i = 0; i < boundaries.length; i++) {
             boundaries[i].classList.remove("youlose")
         }
 
-        
+
     }
 
+    /*Press the start div to reset the scores and reset the game */
     function newGame() {
         document.getElementById("status").innerText = "Begin by moving your mouse over the \"S\".";
         score = 0;
         scores.innerHTML = score;
-        
+
     }
 
+    /*Changin the bounderies colors and status signaling the player's loss and deductating score */
     function lose() {
         if (document.getElementById("status").innerText != "You Lost!" && document.getElementById("status").innerText != "You Won!" && document.getElementById("status").innerText != "You're Out of Bound! Go Back to start") {
             for (var i = 0; i < boundaries.length; i++) {
                 boundaries[i].classList.add("youlose");
-                
+
             }
             document.getElementById("status").innerText = "You Lost!";
-            
+
             score = score - 10;
             scores.innerHTML = `${score}`
         }
     }
 
+    /*Changin the bounderies colors in case of previous loss and status signaling the player's loss and deductating score */
     function win() {
         if (document.getElementById("status").innerText != "You Lost!" && document.getElementById("status").innerText != "You Won!" && document.getElementById("status").innerText != "You're Out of Bound! Go Back to start") {
             document.getElementById("status").innerText = "You Won!";
-            for (var i = 0; i < boundaries.length; i++){
-                boundaries[i].classList.remove("youlose")
-            }
             score = score + 5;
             scores.innerHTML = `${score}`;
         }
 
     }
 
-    function outOfBound(){
+    function outOfBound() {
         document.getElementById("status").innerText = "You're Out of Bound! Go Back to start";
 
     }
 
-    setTimeout(function(){ 
+    setTimeout(function () {
         document.getElementById("status").innerText = "Dont Hit The Boundaries";
     }, 20);
 
