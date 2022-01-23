82602ee9765696f5e130b1c7c9e49fb833e9dcc8
Bilal Achi <Bilal.achi94@gmail.comgit remote add origin https://github.com/Bilalachi/dog_dating_websitegit push -u origin maingit config --global user.email Bilal.achi94@gmail.com>
2022-01-23 05:16:13 +0200

    Start,New Game, Win and Lose functions

--- game.js
+++ game.js
@@ -1,8 +1,23 @@
-window.onload = function() {
-    
-    var start = document.getElementById("start")
+window.onload = function () {
+
+    var start = document.getElementById("start");
     start.onmouseover = startGame;
     start.onclick = newGame;
+
+    var end = document.getElementById("end");
+    end.onmouseover = win;
+
+    var boundaries = document.getElementsByClassName("boundary");
+    for (var i = 0; i < boundaries.length; i++){
+        boundaries.onmouseover = lose;
+    }
+
+
+    var score = 0;
+    var scores =  document.getElementsByClassName("boundary example")[0];
+    scores.innerHTML = `${score}`;
+
+    document.getElementByClassName("example").innerText = score;
 
 
     function startGame() {
@@ -11,6 +26,24 @@ window.onload = function() {
     }
 
     function newGame() {
-        document.getElementById("status").innerText = "Dont Hit The Boundaries";
+        document.getElementById("status").innerText = "Begin by moving your mouse over the \"S\".";
+        score = 0;
+        scores.innerHTML = score;
+    }
+
+    function win() {
+        if (boundaries[1].style.backgroundColor != "red") {
+            document.getElementById("status").innerText = "You Won";
+            score = score + 5;
+            scores.innerHTML = `${score}`;
+        }
+
+    }
+
+    function lose() {
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].classList.add("youlose");
+        }
+        document.getElementById("status").innerText = "You Lost!";
     }
 }
